c5e2ba7a9faaaeb6747965d88cce0bcfabb314a2
NuttWarunyu <NuttWarunyu@gmail.com>
2025-08-17 13:41:43 +0700

    🔧 Remove import from deleted plants/page in index.ts

--- frontend/src/app/index.ts
+++ frontend/src/app/index.ts
@@ -2,7 +2,6 @@
 export * from './page';
 export * from './layout';
 export * from './market/page';
-export * from './plants/page';
 export * from './trends/page';
 export * from './plant-exchange/page';
 export * from './test-language/page';
